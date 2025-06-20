0d8315804231f6b41ab2831dfbab462f7d82e27b
Sanjana Shukla <sanjanashukla5555@gmail.com>
2025-06-20 20:41:34 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -21,7 +21,7 @@ def generate_password():
     password_label=Entry(password_gen,bd="0",bg="gray85",state="readonly",textvariable=password_v)
     password_label.place(x=10,y=140,height=50,width=320)
 
-  #Define a string containing letters, symbols and numbers
+#Define a string containing letters, symbols and numbers
 character_string="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 
@@ -29,25 +29,24 @@ password_gen=Tk()
 password_gen.geometry("350x200")
 password_gen.title("Password Generator")
 
-    #title of the app
+#title of the app
 title_label=Label(password_gen,text="Password Generator",font=('Ubuntu Mono',12))
 title_label.pack()
 
-    #length
+#length
 length_label=Label(password_gen,
 text="Enter length of password: ")
 length_label.place(x=20,y=30)
 length_entry=Entry(password_gen,width=3)
 length_entry.place(x=190,y=30)
 
-    #repeatition
+#repeatition
 repeat_label=Label(password_gen,text="Repetition?1. No repetition , 2. Repetition")
 repeat_label.place(x=20,y=60)
 repeat_entry=Entry(password_gen,width=3)
 repeat_entry.place(x=300,y=60)
 
-
-    #call button
+#call button
 password_button=Button(password_gen,text="Generate Password",command=generate_password)
 password_button.place(x=100,y=100)
 
